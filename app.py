97dddf441ecc06af98915144cb78cd00b8fc1900
MarcoEditia <marcoeditia@gmail.com>
2026-05-05 13:27:23 +0800

    fix comments

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if "messages" not in st.session_state:
 if "session_id" not in st.session_state:
     st.session_state.session_id = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
-# --- SIDEBAR (Lazy Loading) ---
+# --- SIDEBAR  ---
 # Only loads the IDs and Titles
 sidebar_chats = database.get_sidebar_chats()
 
@@ -26,7 +26,7 @@ with st.sidebar:
             st.session_state.session_id = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
             st.rerun()
 
-	# display chat history
+        # display chat history
         st.markdown("Chat")
 
         if sidebar_chats:
@@ -36,7 +36,7 @@ with st.sidebar:
                 
                 # Create the button
                 if st.button(title, key=session_id, width="stretch"):
-                    # ON CLICK: Now we actually hit the database for the heavy messages
+                    # ON CLICK: get the chat history
                     st.session_state.session_id = session_id
                     st.session_state.messages = database.get_chat_messages(session_id)
                     st.rerun() # Refresh the screen
@@ -55,7 +55,7 @@ if prompt := st.chat_input("Ask a question..."):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
     
-    # Mock LLM Response (Replace with your API call later)
+    # Mock LLM Response
     response = f"ThinkTank-ME: {prompt}"
     with st.chat_message("assistant"):
         st.markdown(response)
@@ -63,7 +63,6 @@ if prompt := st.chat_input("Ask a question..."):
     is_new_chat = len(st.session_state.messages) == 2
 
     # Generate Title if it's the first message, otherwise keep the existing one
-    # We use a default title "New Chat" just in case, but overwrite it with the prompt
     chat_title = st.session_state.messages[0]["content"][:30] + "..." if len(st.session_state.messages) > 0 else "New Chat"
 
     # Save to database
